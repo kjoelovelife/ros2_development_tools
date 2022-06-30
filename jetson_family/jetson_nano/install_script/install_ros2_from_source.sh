dd5e1dbd6aec68b28a2a7dca81e48ed8f3ee53a0
wei-chih, lin <weichih.lin@protonmail.com>
2022-06-30 17:30:23 +0800

    Fix: Can use colcon to build ROS2 workspace
    Problem:
      1. Colcon build error.
    
    Reason:
      1. Original cmake version is 3.10.2 in Jetpack 4.6.
    
    Adjust items:
      1. Add function "update_cmake()".

--- jetson_family/jetson_nano/install_script/install_ros2_from_source.sh
+++ jetson_family/jetson_nano/install_script/install_ros2_from_source.sh
@@ -11,22 +11,41 @@ ROS_DISTRO=foxy
 BUILD_ROOT=/opt/ros/$ROS_DISTRO_src
 INSTALL_ROOT=/opt/ros/$ROS_DISTRO
 
+
+
+#######################################
+# Update cmake version from 3.10.2 to 3.20.0
+# Globals:
+#   None
+# Arguments:
+#   install_path
+#######################################
+update_cmake(){
+    sudo apt install -y wget checkinstall
+    cd $1 && wget https://github.com/Kitware/CMake/releases/download/v3.20.0/cmake-3.20.0.tar.gz
+    tar -zvxf cmake-3.20.0.tar.gz
+    cd $1/cmake-3.20.0 && ./bootstrap
+    make -j$(nproc)
+    sudo checkinstall --pkgname=cmake --pkgversion="3.20-custom" --default
+    hash -r
+    apt-cache policy cmake
+    rm $1/cmake-3.20.0.tar.gz
+
+}
+
 #######################################
 # Set locale
-# Globals:
+# Globals: 
 #   None
 # Arguments:
 #   None
 #######################################
 set_locale(){
     locale  # check for UTF-8
-
     sudo apt update && sudo apt install locales
     sudo locale-gen en_US en_US.UTF-8
     sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
     export LANG=en_US.UTF-8
-
-    locale  # verify settings
 }
 
 #######################################
@@ -38,9 +57,12 @@ set_locale(){
 #######################################
 add_ros2_apt_repository(){
     sudo apt install software-properties-common
-    sudo add-apt-repository universe
-    apt-cache policy | grep universe
-    sudo apt update && sudo apt install curl gnupg2 lsb-release
+    sudo apt update && sudo apt install -y \
+        curl \
+        gnupg2 \
+        lsb-release \
+        wget
+    sudo rm -rf /var/lib/apt/lists/*
     wget --no-check-certificate https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc 
     sudo apt-key add ros.asc
     sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
@@ -49,13 +71,14 @@ add_ros2_apt_repository(){
 #######################################
 # Install development tools and ROS tools
 # Globals:
-#   None
+#   BUILD_ROOT
+#   ROS_DISTRO
 # Arguments:
-#   None
+#   install_path
 #######################################
 install_development_tools(){
     sudo apt update
-    sudo apt-get install -y --no-install-recommends \
+    sudo apt-get install -y \
 		build-essential \
 		cmake \
 		git \
@@ -94,13 +117,24 @@ install_development_tools(){
 
 
     # install Fast-RTPS dependencies
-    sudo apt install --no-install-recommends -y \
+    sudo apt install -y \
         libasio-dev \
         libtinyxml2-dev
 
     # install Cyclone DDS dependencies
-    sudo apt install --no-install-recommends -y \
+    sudo apt install -y \
         libcunit1-dev
+
+    # compile yaml-cpp-0.6, which some ROS packages may use (but is not in the 18.04 apt repo)
+    git clone --branch yaml-cpp-0.6.0 https://github.com/jbeder/yaml-cpp $1/yaml-cpp-0.6 && \
+    cd $1/yaml-cpp-0.6 && \
+    mkdir build && \
+    cd build && \
+    cmake -DBUILD_SHARED_LIBS=ON .. && \
+    make -j$(nproc) && \
+    sudo cp libyaml-cpp.so.0.6.0 /usr/lib/aarch64-linux-gnu/ && \
+    sudo ln -s /usr/lib/aarch64-linux-gnu/libyaml-cpp.so.0.6.0 /usr/lib/aarch64-linux-gnu/libyaml-cpp.so.0.6
+
 }
 
 #######################################
@@ -115,6 +149,14 @@ get_ros2_code(){
     sudo mkdir -p $BUILD_ROOT/src && cd $BUILD_ROOT
     sudo sh -c "rosinstall_generator --deps --rosdistro $ROS_DISTRO $1 launch_xml launch_yaml example_interfaces > ros2.$ROS_DISTRO.$1.rosinstall"
     sudo sh -c "vcs import src < ros2.$ROS_DISTRO.$1.rosinstall"
+
+    # download unreleased packages     
+    sudo sh -c "git clone --branch ros2 https://github.com/Kukanani/vision_msgs ${BUILD_ROOT}/src/vision_msgs && \
+    git clone --branch ${ROS_DISTRO} https://github.com/ros2/demos demos && \
+    cp -r demos/demo_nodes_cpp ${BUILD_ROOT}/src && \
+    cp -r demos/demo_nodes_py ${BUILD_ROOT}/src && \
+    rm -r -f demos"
+
 }
 
 #######################################
@@ -130,6 +172,7 @@ rosdep_install(){
     sudo rosdep init
     rosdep update
     rosdep install --from-paths src --ignore-src --rosdistro $ROS_DISTRO -y --skip-keys "console_bridge fastcdr fastrtps rti-connext-dds-5.3.1 urdfdom_headers qt_gui"
+    sudo rm -rf /var/lib/apt/lists/*
 }
 
 
@@ -142,9 +185,11 @@ rosdep_install(){
 #######################################
 build_ros2(){
     sudo mkdir -p $INSTALL_ROOT
-    sudo colcon build --merge-install --install-base $INSTALL_ROOT --symlink-install
-    sudo colcon build --merge-install --install-base $INSTALL_ROOT --symlink-install
+    sudo colcon build --merge-install --install-base $INSTALL_ROOT
+    sudo colcon build --merge-install --install-base $INSTALL_ROOT
     echo "source $INSTALL_ROOT/setup.bash" >> ~/.bashrc 
+    echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
+    #echo "export _colcon_cd_root=~/ros2_install" >> ~/.bashrc
 }
 
 
@@ -156,9 +201,10 @@ build_ros2(){
 #   None
 #######################################
 main(){
+    update_cmake "/home/$USER/src"
     set_locale
     add_ros2_apt_repository
-    install_development_tools
+    install_development_tools "/home/$USER/src"
     get_ros2_code "ros_base"
     rosdep_install
     build_ros2
